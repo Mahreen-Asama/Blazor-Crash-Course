37592407b2d1854b7832d95e938e34dbec284125
Mahreen-Asama <mahreenmehar202@gmail.com>
2022-08-11 14:44:20 +0500

    state mgmt

--- Section8_JSInterop/Section8_JSInterop/Program.cs
+++ Section8_JSInterop/Section8_JSInterop/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Section8_JSInterop;
+using Section8_JSInterop.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<CounterStateService>();          //add that class as a service
 
 await builder.Build().RunAsync();
---
+++ Section8_JSInterop/Section8_JSInterop/Services/CounterStateService.cs
@@ -0,0 +1,20 @@
+﻿namespace Section8_JSInterop.Services
+{
+    public class CounterStateService
+    {
+        private int count;
+        public event Action OnChange;       //event handle of on change
+        public int CounterCount
+        {
+            get { return count; }
+            set
+            {
+                count = value;
+                NotifyChange();             //notify change whenever counter value set new
+            }
+        }
+
+        private void NotifyChange() => OnChange?.Invoke();      //'?' to handle null things
+
+    }
+}
